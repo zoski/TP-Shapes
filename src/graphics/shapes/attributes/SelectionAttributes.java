8fe4921f79071e251bd12fd2fbc431cc91dc875f
DaftSpirit <joris.rat@gmail.com>
2014-04-28 15:15:01 +0200

    maintenant le complément de ShapesAttributes

--- src/graphics/shapes/attributes/SelectionAttributes.java
+++ src/graphics/shapes/attributes/SelectionAttributes.java
@@ -1,11 +1,34 @@
 package graphics.shapes.attributes;
 
-public class SelectionAttributes extends Attribute {
+public class SelectionAttributes extends Attribute 
+{
 
+	public static final String ID = "selection";
+	private boolean selected;
+	
 	@Override
 	public String getId() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.ID;
 	}
-
+	
+	public boolean isSelected()
+	{
+		return this.selected;
+	}
+	
+	public void select()
+	{
+		this.selected = true;
+	}
+	
+	public void unselect()
+	{
+		this.selected = false;
+	}
+	
+	public void toogleSelection()
+	{
+		this.selected = !this.selected;
+	}
 }
