9d50dca164f2095de5b349332b184668b9b5221a
DaftSpirit <joris.rat@gmail.com>
2014-05-08 17:35:12 +0200

    ^pokijuhygtfrde

--- src/graphics/shapes/ui/Editor.java
+++ src/graphics/shapes/ui/Editor.java
@@ -49,12 +49,12 @@ public class Editor extends JFrame
 	{
 		SText st = new SText(new Point(30, 30), "Hello");
 		st.addAttribute(new ColorAttributes(true, true, Color.BLUE, Color.RED));
-		st.addAttribute(new SelectionAttributes());
+		st.addAttribute(new SelectionAttributes(true));
 		this.model.add(st);
 		
 		SRectangle sr = new SRectangle(new Point(70,60),20,30);
 		sr.addAttribute(new ColorAttributes(true, true, Color.BLUE, Color.YELLOW));
-		//sr.addAttribute(new SelectionAttributes());
+		sr.addAttribute(new SelectionAttributes(true));
 		this.model.add(sr);
 //		
 //		SCircle sc = new SCircle(new Point(20,80), 20);
@@ -90,7 +90,115 @@ public class Editor extends JFrame
 		sc.add(r);
 		SCircle c = new SCircle(new Point(150,100),20);
 		c.addAttribute(new ColorAttributes(false,true,Color.BLUE,Color.DARK_GRAY));
+		c.addAttribute(new SelectionAttributes(true));
+		sc.add(c);
+		this.model.add(sc);
+	}
+	
+	public SCollection getModel() {
+		return this.model;
+	}
+	
+	public static void main(String[] args)
+	{
+		Editor self = new Editor();
+		self.pack();
+		self.setVisible(true);
+	}
+}
+package graphics.shapes.ui;
+
+import graphics.shapes.*;
+import graphics.shapes.attributes.ColorAttributes;
+import graphics.shapes.attributes.SelectionAttributes;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+
+import javax.swing.JFrame;
+
+public class Editor extends JFrame
+{
+	ShapesView sview;
+	SCollection model= new SCollection(new Point(10,10));
+	
+	/*
+	 * TODO SCollection avec rect.union done
+	 * TODO Controleur
+	 * TODO Rajouter un point permettant de gérer le déplacement
+	 * TODO Attributs de sélection
+	 * TODO getTarget() dit s'il y Ã  une forme (référence) ou rien s'il n'y Ã  rien
+	 * TODO Finir le draftman
+	 *  
+	 */
+	
+	public Editor()
+	{	
+		super("Shapes Editor");
+
+		this.addWindowListener(new java.awt.event.WindowAdapter()
+		{
+			public void windowClosing(java.awt.event.WindowEvent evt)
+			{
+				System.exit(0);
+			}
+		});
+		
+		this.buildModel();
+		
+		this.sview = new ShapesView(this.model);
+		this.sview.setPreferredSize(new Dimension(450,450));
+		this.getContentPane().add(this.sview, java.awt.BorderLayout.CENTER);
+	}
+
+	
+	private void buildModel()
+	{
+		SText st = new SText(new Point(30, 30), "Hello");
+		st.addAttribute(new ColorAttributes(true, true, Color.BLUE, Color.RED));
+		st.addAttribute(new SelectionAttributes(true));
+		this.model.add(st);
+		
+		SRectangle sr = new SRectangle(new Point(70,60),20,30);
+		sr.addAttribute(new ColorAttributes(true, true, Color.BLUE, Color.YELLOW));
+		sr.addAttribute(new SelectionAttributes(true));
+		this.model.add(sr);
+//		
+//		SCircle sc = new SCircle(new Point(20,80), 20);
+//		sc.addAttribute(new ColorAttributes(true, true, Color.GREEN, Color.CYAN));
+//		this.model.add(sc);
+//		
+		System.out.println(model);
+		
+//		this.model = new SCollection();
+//		this.model.addAttributes(new SelectionAttributes());
+//		
+//		SRectangle r = new SRectangle(new Point(10,10),20,30);
+//		r.addAttributes(new ColorAttributes(true,false,Color.BLUE,Color.BLUE));
+//		r.addAttributes(new SelectionAttributes());
+//		this.model.add(r);
+//		
+//		SCircle c = new SCircle(new Point(100,100),10);
+//		c.addAttributes(new ColorAttributes(false,true,Color.BLUE,Color.BLUE));
 //		c.addAttributes(new SelectionAttributes());
+//		this.model.add(c);
+//		
+//		SText t= new SText(new Point(100,100),"hello");
+//		t.addAttributes(new ColorAttributes(true,true,Color.YELLOW,Color.BLUE));
+//		t.addAttributes(new FontAttributes());
+//		t.addAttributes(new SelectionAttributes());
+//		this.model.add(t);
+//		
+		SCollection sc = new SCollection(new Point(20,20));
+//		sc.addAttributes(new SelectionAttributes());
+		SRectangle r= new SRectangle(new Point(20,30),30,30);
+		r.addAttribute(new ColorAttributes(true,false,Color.MAGENTA,Color.BLUE));
+//		r.addAttribute(new SelectionAttributes());
+		sc.add(r);
+		SCircle c = new SCircle(new Point(150,100),20);
+		c.addAttribute(new ColorAttributes(false,true,Color.BLUE,Color.DARK_GRAY));
+		c.addAttribute(new SelectionAttributes(true));
 		sc.add(c);
 		this.model.add(sc);
 	}
