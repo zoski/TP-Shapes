c75c384bfb5b6e3a3e74569ad08a8ea012c91d5e
DaftSpirit <joris.rat@gmail.com>
2014-05-08 17:04:02 +0200

    coucou

--- src/graphics/shapes/ui/Editor.java
+++ src/graphics/shapes/ui/Editor.java
@@ -49,11 +49,12 @@ public class Editor extends JFrame
 	{
 		SText st = new SText(new Point(30, 30), "Hello");
 		st.addAttribute(new ColorAttributes(true, true, Color.BLUE, Color.RED));
-		//st.addAttribute(new SelectionAttributes());
+		st.addAttribute(new SelectionAttributes());
 		this.model.add(st);
 		
 		SRectangle sr = new SRectangle(new Point(70,60),20,30);
 		sr.addAttribute(new ColorAttributes(true, true, Color.BLUE, Color.YELLOW));
+		//sr.addAttribute(new SelectionAttributes());
 		this.model.add(sr);
 //		
 //		SCircle sc = new SCircle(new Point(20,80), 20);
